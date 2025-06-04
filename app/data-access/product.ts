3c5f2504a655e9d33b2b46790d50d71c5c3468ba
Érick Fernandes <erickfernandess2@gmail.com>
2025-06-04 16:24:43 -0500

    Página de Produto

--- app/data-access/product.ts
+++ app/data-access/product.ts
@@ -5,6 +5,10 @@ export async function createProduct(product: Prisma.ProductCreateInput): Promise
     return await prisma.product.create({ data: product });
 }
 
+export async function getProduct(id: string){
+    return await prisma.product.findFirst({ where: { id: id } });
+}
+
 export async function getProductByLegacyId(legacyId: number, legacyOriginDatabase: string): Promise<Product | null>{
     return await prisma.product.findFirst({
         where: {
